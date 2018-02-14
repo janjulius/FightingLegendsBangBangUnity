2a8c44e704357e577e0e1d26187450cf58131f8b
kiprok <arthur.vandenbroek@outlook.com>
2018-02-14 13:31:32 +0100

    ping lol

---
+++ fighting legends bang bang/Assets/pingTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pingTest : MonoBehaviour {
+
+    float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = Color.white;
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        float ping = PhotonNetwork.networkingPeer.RoundTripTime;
+        string pingTest = string.Format("\nping: {0}", ping);
+        GUI.Label(rect, text+pingTest, style);
+    }
+}
