f04f34b4bb35e7ada4dccc5c1b520d2184fff7d6
kiprok <arthur.vandenbroek@outlook.com>
2018-02-22 16:13:20 +0100

    rocky soundsssss

--- fighting legends bang bang/Assets/scripts/other/Ghost.cs
+++ fighting legends bang bang/Assets/scripts/other/Ghost.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    internal int ghostAudio = 3;
 
     void Start()
     {
+        PlayerNetwork.Instance.photonView.RPC("PlaySound", PhotonTargets.All, ghostAudio);
         var pho = GetComponent<PhotonView>();
         PhotonPlayer p = pho.owner;
         var col = new Color((float)p.CustomProperties["pColorR"], (float)p.CustomProperties["pColorG"], (float)p.CustomProperties["pColorB"]);
