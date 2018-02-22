4c5f0f6f4957ee724b918e49eca3a10a239a1907
JanJulius <lang1600@student.nhl.nl>
2018-02-22 16:50:28 +0100

    Added pirateship lvl, lowered brightenss on stadium level

---
+++ fighting legends bang bang/Assets/scripts/levels/TextureOffsetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureOffsetter : MonoBehaviour {
+
+    public float scrollSpeedX = 0.5f;
+    public float scrollSpeedY = 0;
+    public string name = "_MainTex";
+    private Material r;
+
+    void Start()
+    {
+        r = GetComponent<SkinnedMeshRenderer>().materials[0];
+    }
+
+    void Update()
+    {
+        r.SetTextureOffset(name, new Vector2(Time.time * scrollSpeedX, Time.time * scrollSpeedY));
+    }
+}
