5efe77162024a9050b4a746bb4cb0e2af10e13d8
JanJulius <lang1600@student.nhl.nl>
2018-02-22 19:42:29 +0100

    some more pirateship adjustments and started working on boomstronk

--- fighting legends bang bang/Assets/scripts/character/Boomstronk.cs
+++ fighting legends bang bang/Assets/scripts/character/Boomstronk.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boomstronk : MonoBehaviour {
+public class Boomstronk : Character {
 
 	// Use this for initialization
 	void Start () {
