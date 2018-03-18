b6c1c599a6b0964769e3c7a004646ec6666bf511
kiprok <arthur.vandenbroek@outlook.com>
2018-03-18 17:23:45 +0100

    fixed knockback and ghost script

--- fighting legends bang bang/Assets/scripts/player/Health.cs
+++ fighting legends bang bang/Assets/scripts/player/Health.cs
@@ -118,7 +118,7 @@ public class Health : MonoBehaviour
 
                 if (!isKnockBackImmune)
                 {
-                    pb.AddKnockBack(dir, 10 + this.dmg+this.dmg);
+                    pb.AddKnockBack(dir, 10 + this.dmg);
                 }
             }
             else
