1a75a19134caa31345f8765705adbe444cdb842a
JanJulius <lang1600@student.nhl.nl>
2018-06-30 00:00:58 +0200

    Tjeerd assets and animations

---
+++ fighting legends bang bang/Assets/scripts/character/Tjeerd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tjeerd : Character
+{
+    public Tjeerd()
+    {
+        name = "Tjeerd";
+        nameAfter = "the Rugby God";
+        AttackCooldown = 0.5f;
+        SwingCooldown = 0.1f;
+        BasicAttackDamage = 10;
+        specialCounterThreshHold = 100;
+        rangeModifier = 1;
+    }
+    public override void Attack(Vector2 dir)
+    {
+        base.Attack(dir);
+    }
+
+    public override void SpecialAttack()
+    {
+        print("special pogchamp");
+    }
+}
